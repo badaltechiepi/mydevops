0cfdc7ed8e6ed30ff08d8b7c99cecfaf5b4d13ab
Ubuntu <kubectl@monitoring-devops.bwabjhjbkaeulai4yf3gkwmceg.cx.internal.cloudapp.net>
2020-09-27 14:52:27 +0000

    modified the read script

--- read.sh
+++ read.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-read -p "Enter the value of A:"A
-read -p "Enter the value of B:"B
+read -p "Enter the value of A:" A
+read -p "Enter the value of B:" B
 
 echo "The value of A is $A"
 echo "The value of B is $B"
